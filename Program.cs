1bbbfcb0e93e2afed74e5357522206e240da72a7
Javachin0 <38870219+Javachin0@users.noreply.github.com>
2020-06-07 13:52:32 +0100

    Init commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+
+namespace Alpha_Top10Words
+{
+    class Program
+    {
+        
+        private static string directory = Directory.GetCurrentDirectory();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Put text file in application .exe Root directory: " + directory);
+            try
+            {
+                DisplayWordCount();    
+            }       
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+
+        private static void DisplayWordCount()
+        {
+            string file = TextUtil.ReadFile();
+            var words = TextUtil.ExtractWords(file);
+            //Display Top 10 Results
+            var results = TextUtil.FindDuplicates(words)
+                            .OrderByDescending(count => count.Value)
+                            .Take(10);
+            int i = 1;
+            foreach (var result in results)
+            {
+                Console.WriteLine($"{i}. '{result.Key}' appears: {result.Value} times");
+                i++;
+            }
+        }
+    }
+}
---
+++ TextUtil.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Alpha_Top10Words
+{
+    public static class TextUtil
+    {
+        private const string wordPattern = @"\b\w+\b";
+        internal static Dictionary<string, int> FindDuplicates(List<string> words)
+        {
+            return words.GroupBy(element => element, StringComparer.InvariantCultureIgnoreCase) //group all identical words, regardless of case
+                        .Where(element => element.Count() > 1)
+                        .ToDictionary(x => x.Key, y => y.Count());
+        }
+
+        internal static string ReadFile()
+        {
+            var txtFile = Directory.GetFiles(Directory.GetCurrentDirectory(), "*.txt");
+            if(txtFile.Count() == 1)
+            {
+                Console.WriteLine("Reading Text file..");
+                return File.ReadAllText(txtFile[0]); 
+            }              
+            else
+                throw new ArgumentException("Incorrect number of text files in Root: " + txtFile.Count());
+        }
+        internal static List<string> ExtractWords(string text)
+        {
+             Regex regex = new Regex(wordPattern);
+             MatchCollection matches = regex.Matches(text);
+             Console.WriteLine("Word Count: " + matches.Count);
+             return matches.Cast<Match>()
+                           .Select(match => match.Value)
+                           .ToList();
+        }
+    }
+}
